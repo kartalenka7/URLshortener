013e1109c17831633171acc23b98a55b699050ac
Elena Khramkova <khramkovase@gmail.com>
2023-02-11 13:31:21 +0300

    first increment is completed

---
+++ cmd/client/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// адрес сервиса
+	endpoint := "http://localhost:8080/"
+	// контейнер данных для запроса
+	data := url.Values{}
+	// приглашение в консоли
+	fmt.Println("Введите длинный URL")
+	// открываем потоковое чтение из консоли
+	reader := bufio.NewReader(os.Stdin)
+	// читаем строку из консоли
+	long, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	long = strings.TrimSuffix(long, "\n")
+	// заполняем контейнер данными
+	data.Set("url", long)
+	// конструируем HTTP-клиент
+	client := &http.Client{}
+	// конструируем запрос
+
+	// запрос методом POST должен, кроме заголовков, содержать тело
+	// тело должно быть источником потокового чтения io.Reader
+	// в большинстве случаев отлично подходит bytes.Buffer
+	request, err := http.NewRequest(http.MethodPost, endpoint, bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// в заголовках запроса сообщаем, что данные кодированы стандартной URL-схемой
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+	// отправляем запрос и получаем ответ
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// печатаем код ответа
+	fmt.Println("Статус-код ", response.Status)
+	defer response.Body.Close()
+	// читаем поток из тела ответа
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// и печатаем его
+	fmt.Println(string(body))
+
+	//запрос методом GET
+	shortURL := string(body)
+	URL, urlParseErr := url.Parse(shortURL)
+	fmt.Println(URL)
+	if urlParseErr != nil {
+		fmt.Println(urlParseErr)
+		os.Exit(2)
+	}
+
+	/* shortURL = strings.TrimSuffix(shortURL, "\n")
+	req, err := http.NewRequest(http.MethodGet, shortURL, nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Статус-код ", resp.Status)
+	longURL := resp.Header.Get("Location")
+	fmt.Println(resp.Header)
+	fmt.Println("Длинный URL", longURL) // печатаем код ответа
+
+	defer resp.Body.Close() */
+
+}
--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,71 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"strings"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// Генерирование короткой ссылки
+func randStringBytes(n int) string {
+	link := make([]byte, n)
+	for i := range link {
+		link[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(link)
+}
+
+type SavedLinks struct {
+	LinksMap map[string]string
+}
+
+func (s SavedLinks) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// проверяем, каким методом получили запрос
+	switch r.Method {
+	case "POST":
+		//читаем строку URL из body
+		b, err := io.ReadAll(r.Body)
+		// обрабатываем ошибку
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		// получаем токен
+		sToken := randStringBytes(10)
+		url := string(b)
+		// записываем связку короткий url - длинный url
+		s.LinksMap[sToken] = url
+		// возвращаем ответ с кодом 201
+		w.WriteHeader(201)
+		// пишем в тело ответа сокращенный URL
+		fmt.Print(w, sToken)
+
+	case "GET":
+		shortURL := r.URL.String()
+		shortToken := strings.Replace(shortURL, "/", "", -1)
+		longURL := s.LinksMap[shortToken]
+		w.Header().Set("Location", longURL)
+		// возвращаем ответ с кодом 307
+		w.WriteHeader(307)
+	}
+}
+
+func main() {
+	savedLinks := make(map[string]string)
+	// маршрутизация запросов обработчику
+	handler1 := SavedLinks{
+		LinksMap: savedLinks,
+	}
+	server := &http.Server{
+		Handler: handler1,
+		Addr:    "localhost:8080",
+	}
+	// Запуск сервера
+	log.Fatal(server.ListenAndServe())
+
+}
