b7140cf2524240320fc143d2f9d26e44d846d439
Elena Khramkova <khramkovase@gmail.com>
2023-02-18 01:09:57 +0300

    increment3 attempt 2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,119 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
-	"math/rand"
 	"net/http"
 
-	//"strings"
-
-	"github.com/go-chi/chi/v5"
+	utils "example.com/shortener/cmd/utils"
+	handlers "example.com/shortener/internal/app/handlers"
+	storage "example.com/shortener/internal/app/storage"
 )
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-// Генерирование короткой ссылки
-func randStringBytes(n int) string {
-	link := make([]byte, n)
-	for i := range link {
-		link[i] = letterBytes[rand.Intn(len(letterBytes))]
-	}
-	return string(link)
-}
-
-type SavedLinks struct {
-	LinksMap map[string]string
-	gToken   string
-}
-
-/* func (s SavedLinks) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.String())
-	// проверяем, каким методом получили запрос
-	switch r.Method {
-	case "POST":
-		//читаем строку URL из body
-		b, err := io.ReadAll(r.Body)
-		url := string(b)
-		// обрабатываем ошибку
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		// получаем токен
-		sToken := s.gToken
-		// записываем связку короткий url - длинный url
-		s.LinksMap[sToken] = url
-		// возвращаем ответ с кодом 201
-		w.WriteHeader(201)
-		// пишем в тело ответа сокращенный URL
-		sToken = "http://localhost:8080/" + sToken
-		fmt.Fprint(w, sToken)
-
-	case "GET":
-		shortURL := r.URL.String()
-		shortToken := strings.Replace(shortURL, "/", "", -1)
-		longURL := s.LinksMap[shortToken]
-		w.Header().Set("Location", longURL)
-		// возвращаем ответ с кодом 307
-		w.WriteHeader(307)
-	}
-} */
+var (
+	localAddr = "localhost:8080"
+)
 
 func main() {
-	savedLinks := make(map[string]string)
-	// маршрутизация запросов обработчику
-	links := SavedLinks{
-		LinksMap: savedLinks,
-		gToken:   randStringBytes(10),
-	}
-	/*server := &http.Server{
-		Handler: handler1,
-		Addr:    "localhost:8080",
-	}
-	log.Fatal(server.ListenAndServe())*/
-	router := NewRouter(links)
-	log.Fatal(http.ListenAndServe("localhost:8080", router))
-
-}
-
-func NewRouter(s SavedLinks) chi.Router {
-	// определяем роутер chi
-	r := chi.NewRouter()
-	// создадим суброутер, который будет содержать две функции
-	r.Route("/", func(r chi.Router) {
-		r.Post("/", func(rw http.ResponseWriter, req *http.Request) {
-			fmt.Println(req.Method)
-			fmt.Println(req.URL)
-			//читаем строку URL из body
-			b, err := io.ReadAll(req.Body)
-			url := string(b)
-			// обрабатываем ошибку
-			if err != nil {
-				http.Error(rw, err.Error(), 500)
-				return
-			}
-			// получаем токен
-			sToken := s.gToken
-			// записываем связку короткий url - длинный url
-			s.LinksMap[sToken] = url
-			// возвращаем ответ с кодом 201
-			rw.WriteHeader(201)
-			// пишем в тело ответа сокращенный URL
-			sToken = "http://localhost:8080/" + sToken
-			fmt.Fprint(rw, sToken)
-		})
-		r.Get("/{id}", func(rw http.ResponseWriter, req *http.Request) {
-			fmt.Println(req.Method)
-			fmt.Println(req.URL)
-			shortURL := chi.URLParam(req, "id")
-			longURL := s.LinksMap[shortURL]
-			rw.Header().Set("Location", longURL)
-			// возвращаем ответ с кодом 307
-			rw.WriteHeader(307)
-		})
-	})
-
-	return r
+	storage := storage.GetStorage(make(map[string]string))
+	router := handlers.NewRouter(*storage, utils.RandStringBytes(10))
+	log.Fatal(http.ListenAndServe(localAddr, router))
 }
--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -7,149 +7,129 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"errors"
+
+	handlers "example.com/shortener/internal/app/handlers"
+	storage "example.com/shortener/internal/app/storage"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestPOST(t *testing.T) {
-	links := SavedLinks{
-		LinksMap: map[string]string{
-			" ": " ",
-		},
-		gToken: "AsDfGhJkLl",
-	}
+func TestEndpoints(t *testing.T) {
 
-	r := NewRouter(links)
-	ts := httptest.NewServer(r)
-	defer ts.Close()
-
-	statusCode, body, _ := testRequest(t, ts, "POST", "/")
-	assert.Equal(t, 201, statusCode)
-	assert.Equal(t, "http://localhost:8080/AsDfGhJkLl", body)
-}
-
-/*  func TestGET(t *testing.T) {
-	links := SavedLinks{
-		LinksMap: map[string]string{
-			"AsDfGhJkLl": "http://testtest/AsDfGhJkLl",
-		},
-		gToken: " ",
-	}
-
-	r := NewRouter(links)
-	ts := httptest.NewServer(r)
-	defer ts.Close()
-
-	statusCode, _, location := testRequest(t, ts, "GET", "/AsDfGhJkLl")
-	assert.Equal(t, 307, statusCode)
-	assert.Equal(t, "http://testtest/AsDfGhJkLl", location)
-}  */
-
-func testRequest(t *testing.T, ts *httptest.Server, method, path string) (int, string, string) {
-	fmt.Println(ts.URL + path)
-	req, err := http.NewRequest(method, ts.URL+path, nil)
-	assert.NoError(t, err)
-
-	resp, err := http.DefaultClient.Do(req)
-	assert.NoError(t, err)
-
-	respBody, err := io.ReadAll(resp.Body)
-	assert.NoError(t, err)
-
-	defer resp.Body.Close()
-
-	return resp.StatusCode, string(respBody), string(resp.Header.Get("Location"))
-}
-
-/* func TestPOST(t *testing.T) {
 	type want struct {
 		statusCode int
-		URL        string
-	}
-	testsPost := []struct {
-		name    string
-		links   SavedLinks
-		want    want
-		request string
-	}{
-		{
-			name: "POST test",
-			links: SavedLinks{
-				LinksMap: map[string]string{
-					" ": " ",
-				},
-				gToken: "AsDfGhJkLl",
-			},
-			want: want{
-				statusCode: 201,
-				URL:        "http://localhost:8080/AsDfGhJkLl",
-			},
-			request: "/",
-		},
-	}
-
-	for _, tt := range testsPost {
-		t.Run(tt.name, func(t *testing.T) {
-			request := httptest.NewRequest(http.MethodPost, tt.request, nil)
-			w := httptest.NewRecorder()
-			handler1 := SavedLinks{
-				LinksMap: tt.links.LinksMap,
-				gToken:   tt.links.gToken,
-			}
-			h := http.HandlerFunc(handler1.ServeHTTP)
-			h(w, request)
-			result := w.Result()
-			assert.Equal(t, tt.want.statusCode, result.StatusCode)
-			defer result.Body.Close()
-			body, err := io.ReadAll(result.Body)
-			if err != nil {
-				os.Exit(1)
-			}
-			URL := string(body)
-			assert.Equal(t, tt.want.URL, URL)
-		})
-	}
-}
-
-func TestGET(t *testing.T) {
-	type want struct {
-		statusCode int
-		URL        string
+		URLshort   string
+		err        string
 	}
 	testsGet := []struct {
 		name    string
-		links   SavedLinks
+		storage storage.StorageLinks
+		gToken  string
 		want    want
+		method  string
 		request string
 	}{
 		{
-			name: "POST test",
-			links: SavedLinks{
+			name: "POST positive test",
+			storage: storage.StorageLinks{
 				LinksMap: map[string]string{
-					"AsDfGhJkLl": "http://localhost:8080/AsDfGhJkLl",
+					" ": " ",
 				},
 			},
+			gToken: "AsDfGhJkLl",
 			want: want{
-				statusCode: 307,
-				URL:        "http://localhost:8080/AsDfGhJkLl",
+				statusCode: http.StatusCreated,
+				URLshort:   "http://localhost:8080/AsDfGhJkLl",
+				err:        "",
 			},
+			method:  http.MethodPost,
+			request: "/",
+		},
+		{
+			name: "POST negative test",
+			storage: storage.StorageLinks{
+				LinksMap: map[string]string{
+					"AsDfGhJkLl": "http://test/AsDfGhJkLl",
+				},
+			},
+			gToken: "AsDfGhJkLl",
+			want: want{
+				statusCode: http.StatusInternalServerError,
+				URLshort:   "Link already exists\n",
+				err:        "",
+			},
+			method:  http.MethodPost,
+			request: "/",
+		},
+		{
+			name: "GET positive test",
+			storage: storage.StorageLinks{
+				LinksMap: map[string]string{
+					"AsDfGhJkLl": "https://www.youtube.com",
+				},
+			},
+			gToken: "",
+			want: want{
+				statusCode: http.StatusTemporaryRedirect,
+				URLshort:   "",
+				err:        "Get \"https://www.youtube.com\": Redirect",
+			},
+			method:  http.MethodGet,
 			request: "/AsDfGhJkLl",
+		},
+		{
+			name: "GET negative test",
+			storage: storage.StorageLinks{
+				LinksMap: map[string]string{
+					"AsDfGhJkLl": "https://www.youtube.com",
+				},
+			},
+			gToken: "",
+			want: want{
+				statusCode: http.StatusMethodNotAllowed,
+				URLshort:   "",
+				err:        "",
+			},
+			method:  http.MethodGet,
+			request: "/",
 		},
 	}
 
 	for _, tt := range testsGet {
 		t.Run(tt.name, func(t *testing.T) {
-			request := httptest.NewRequest(http.MethodGet, tt.request, nil)
-			w := httptest.NewRecorder()
-			handler1 := SavedLinks{
-				LinksMap: tt.links.LinksMap,
+			r := handlers.NewRouter(tt.storage, tt.gToken)
+			ts := httptest.NewServer(r)
+			defer ts.Close()
+			fmt.Println(tt.method, tt.request)
+			statusCode, body, err := testRequest(t, ts, tt.method, tt.request)
+			assert.Equal(t, tt.want.statusCode, statusCode)
+			assert.Equal(t, tt.want.URLshort, body)
+			if err != nil {
+				fmt.Println(err.Error())
+				assert.Equal(t, tt.want.err, err.Error())
 			}
-			h := http.HandlerFunc(handler1.ServeHTTP)
-			h(w, request)
-			result := w.Result()
-			defer result.Body.Close()
-			assert.Equal(t, tt.want.statusCode, result.StatusCode)
-			URL := result.Header.Get("Location")
-			assert.Equal(t, tt.want.URL, URL)
 		})
 	}
-} */
+}
+
+func testRequest(t *testing.T, ts *httptest.Server, method, request string) (int, string, error) {
+	var respBody []byte
+	var err error
+
+	req, err := http.NewRequest(method, ts.URL+request, nil)
+	require.NoError(t, err)
+
+	client := new(http.Client)
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return errors.New("Redirect")
+	}
+	resp, err := client.Do(req)
+
+	if err == nil {
+		respBody, err = io.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		require.NoError(t, err)
+	}
+	return resp.StatusCode, string(respBody), err
+}
